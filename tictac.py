fc016b4d8c604470a323585b6b6fa0855b755d27
Karuna Grewal <karunagrewal98@gmail.com>
2017-11-22 22:31:50 +0530

    added board validity test

--- tictac.py
+++ tictac.py
@@ -1,3 +1,17 @@
+def winCheck(board):
+    for i in range(3):
+        if len(set(board[i*3:i*3+3])) is  1 and board[i*3] is not 'c': return True
+    
+    for i in range(3):
+       if (board[i] is board[i+3]) and (board[i] is  board[i+6]) and board[i] is not 'c':
+           return True
+    
+    if board[0] is board[4] and board[4] is board[8] and board[4] is not 'c':
+        return  True
+    if board[2] is board[4] and board[4] is board[6] and board[4] is not 'c':
+        return  True
+    return False
+
 def generateBoard(board,index,fboard):
 	if index!=9 :
 		for i in range(3):
@@ -24,20 +38,15 @@ def generateBoard(board,index,fboard):
 				generateBoard(board,indexc,fboard)
 	else :
 		return 0
-	
-def winCheck(board):
-    for i in range(3):
-        if len(set(board[i*3:i*3+3])) is  1 and board[i*3] is not 'c': return True
-    
-    for i in range(3):
-       if (board[i] is board[i+3]) and (board[i] is  board[i+6]) and board[i] is not 'c':
-           return True
-    
-    if board[0] is board[4] and board[4] is board[8] and board[4] is not 'c':
-        return  True
-    if board[2] is board[4] and board[4] is board[6] and board[4] is not 'c':
-        return  True
-    return False
+
+def checkValidity(board):
+	if board.count("x") != board.count("o")	:
+		f2.write("invalid \n")
+		return 0
+	if winCheck(board):
+		f2.write("has won \n")
+		return 0
+	return 1
 
 def nextMove(player,board) :
 	nextPlayer = 'o' if player == 'x' else 'x'
@@ -71,20 +80,36 @@ def nextMove(player,board) :
 	elif player is "o" :
 			minele=min(list_scores)
 			return minele,list_empty_spaces[list_scores.index(minele)]
-"""
-f2 =open("./output.txt","w+")
-f= open("./boards.txt","r")
-for line in f:
-	temp_board=list(line)
-	temp_board.pop(9)
-	print temp_board
-	score,move=nextMove('x',temp_board)
-	print move
-	f2.write(str(move)+'\n')
-f2.close()
-f.close()
-"""
+
+
+
 board=list("ccccccccc")
 fboard = open("./allBoards.txt","w+")
 generateBoard(board,0,fboard)
 fboard.close()
+
+f2 =open("./output.txt","w+")
+f= open("./allBoards.txt","r")
+
+
+for line in f:
+	temp_board=[]
+	input_from_file=[]
+	input_from_file=line
+	print input_from_file
+	print len(input_from_file)
+	for i in range(len(input_from_file)):
+		if input_from_file[i] is 'x' or input_from_file[i] is 'o' or input_from_file[i] is 'c'	:
+			temp_board.append(input_from_file[i])
+	print len(temp_board)
+	print temp_board
+
+	if checkValidity(temp_board)==0:
+		continue
+	score,move=nextMove('x',temp_board)
+	print move
+	f2.write(str(move)+'\n')
+
+f2.close()
+f.close()
+
